3490560a3b9a48b026dfa377ca5364b57b9f0eb8
Feltrin Emanuele <Emanuelefeltrin19971010@gmail.com>
2019-10-30 12:46:50 +0100

    Configurazione programma

---
+++ FeltrinEmanuele_Gioco15/FeltrinEmanuele_Gioco15/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FeltrinEmanuele_Gioco15
+//
+//  Created by EMANUELE FELTRIN on 30/10/2019.
+//  Copyright © 2019 EMANUELE FELTRIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
